616f4fc8816bcc3ed1326d153ebd81d01ed32ac6
Annie Liao <liaowow@gmail.com>
2020-04-11 17:34:17 -0400

    added other solutions to newYearChaos

--- newYearChaos.js
+++ newYearChaos.js
@@ -23,6 +23,7 @@ function readLine() {
 }
 
 // Complete the minimumBribes function below.
+/*** MY SOLUTION ***/
 function minimumBribes(q) {
   let finalArr = q.slice()
   let sortedArr = q.sort((a, b) => a - b)
@@ -40,6 +41,59 @@ function minimumBribes(q) {
     }
   }
   console.log(numOfBribes)
+}
+
+/*** SOLUTION #1 via Medium ***/
+/*** https://levelup.gitconnected.com/array-code-challenge-breakdown-8799f903d9cc ***/
+function minimumBribes1(q) {
+    let swaps = 0
+    for (let i = 0; i < q.length; i++) {
+      let bribes = q[i] - (i + 1)
+      let maxAdvance = q[i] - 2 > 0 ? q[i] - 2 : 0
+        if (bribes > 2) {
+          console.log('Too chaotic')
+          return
+        }
+      for (let j = maxAdvance; j < i; j++) {
+        if (q[j] > q[i]) swaps++
+      }
+    }
+    console.log(swaps)
+}
+  
+  
+/*** SOLUTION #2 via GitHub ***/
+/*** https://github.com/Amjad-H-Ali/New-Year-Chaos/blob/master/JS/app.js ***/
+function minimumBribes2(q) {
+    let counter = 0
+    for (let i = q.length - 1; i >= 0; i--) {
+        if (q[i] - (i + 1) > 2) {
+            console.log("Too chaotic")
+            return
+        }
+        for (let j = Math.max(0, q[i] - 2); j < i; j++ ) 
+            if(q[j] > q[i]) {
+                counter++
+            }
+    }
+    console.log(counter)
+}
+  
+/*** SOLUTION #3 via StackExchange ***/
+/*** https://codereview.stackexchange.com/questions/217867/new-year-chaos-javascript-needs-to-be-sped-up ***/
+function minimumBribes3(queue) {
+    var bribes = 0, i, j;
+    for (i = 0; i < queue.length; i++) {
+        const pos = queue[i], at = i + 1;
+        if (pos - at > 2) { 
+        console.log("Too chaotic") 
+        return
+        } 
+        for (j = pos - 2; j < i; j++) {
+            if (queue[j] > pos) { bribes++ }
+        }
+    } 
+    console.log(bribes)
 }
 
 function main() {
