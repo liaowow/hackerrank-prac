e1cdf1f0d62bf68edc0d5034f72b16282c416fab
Annie Liao <liaowow@gmail.com>
2020-05-04 14:35:08 -0400

    added test cases and explainers on spiralMatrix

--- spiralMatrix.js
+++ spiralMatrix.js
@@ -51,6 +51,9 @@ function matrix(n) {
     return results
 }
 
+// resource for NxN spiral matrix:
+// https://codereview.stackexchange.com/questions/213931/print-out-n-by-n-spiral-matrix-in-javascript  
+
 /* Given a 2D array, print it in spiral form */
 // ES6 Solution
 function spiral(matrix) {
@@ -58,11 +61,26 @@ function spiral(matrix) {
 
     while (matrix.length) {
         arr.push(
+            // remove && return 1st row
             ...matrix.shift(),
+            // remove && return last element of the remaining rows
             ...matrix.map(a => a.pop()),
+            // remove last row && return those elements in reverse order
             ...matrix.pop().reverse(),
+            // remove 1st element of the remaining rows && return those elements in reverse order
             ...matrix.map(a => a.shift()).reverse()
         );
     }
     return arr;
 }
+
+// resource for traversing matrix in spiral order: 
+// https://stackoverflow.com/questions/30906366/spiral-traversal-of-a-matrix-recursive-solution-in-javascript
+// https://www.geeksforgeeks.org/print-a-given-matrix-in-spiral-form/
+
+/******* TEST CASE(S) *******/
+matrix(4)
+spiral([[1,2,3,4],
+        [5,6,7,8],
+        [9,10,11,12],
+        [13,14,15,16]])
