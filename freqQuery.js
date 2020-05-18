2d72107617512c54f6efe08bf72d823dce33180e
Annie Liao <liaowow@gmail.com>
2020-05-18 15:37:22 -0400

    added my solution of freqQuery

--- freqQuery.js
+++ freqQuery.js
@@ -23,10 +23,52 @@ function readLine() {
 }
 
 // Complete the freqQuery function below.
+/*** MY SOLUTION: did not pass large data cases ***/
 function freqQuery(queries) {
-    
-
-}
+    let result = []
+    let newArr = []
+  
+    queries.forEach(pair => {
+      if (pair[0] === 1) {
+        newArr.push(pair[1])
+      }
+  
+      if (pair[0] === 2) {
+        if (newArr.includes(pair[1])) {
+          // get index of pair[1]
+          const indexFound = newArr.indexOf(pair[1])
+          // remove pair[1] via splice
+          newArr.splice(indexFound, 1)
+        }
+      }
+  
+      if (pair[0] === 3) {
+        let dict = {}
+        
+        newArr.forEach(val => {
+          if (!dict[val]) {
+            dict[val] = 1
+          } else {
+            dict[val]++
+          }
+        })
+  
+        let freqCheck = 0
+  
+        for (let key in dict) {
+          if (dict[key] === pair[1]) {
+            freqCheck++
+          }
+        }
+  
+        freqCheck === 0 ? result.push(0) : result.push(1)
+  
+      }
+    })
+  
+    return result
+  
+  }
 
 function main() {
     const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
