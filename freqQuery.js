ac2671b7aecbd6678308aa9f6fd47ccb8e5ef0b3
Annie Liao <liaowow@gmail.com>
2020-05-18 16:03:04 -0400

    added a freqQuery solution that passes all tests

--- freqQuery.js
+++ freqQuery.js
@@ -44,7 +44,6 @@ function freqQuery(queries) {
   
       if (pair[0] === 3) {
         let dict = {}
-        
         newArr.forEach(val => {
           if (!dict[val]) {
             dict[val] = 1
@@ -54,21 +53,52 @@ function freqQuery(queries) {
         })
   
         let freqCheck = 0
-  
         for (let key in dict) {
           if (dict[key] === pair[1]) {
             freqCheck++
           }
-        }
-  
+        }  
         freqCheck === 0 ? result.push(0) : result.push(1)
-  
       }
-    })
-  
+    })  
     return result
   
+}
+
+/*** Solution#1 ***/
+function freqQueryAll(arr) {
+  const result = [];
+  const dict = {};
+  const freq = []; // keep an extra list of frequencies
+  /* 
+  I used a simple array where the index is the number of occurencies, for example: 
+  [0,2,3] means that there are 2 values which occur twice and 3 values that occur 3 times (dictionary in this case would be something like: {12: 2, 14: 3})
+  This way in case 3 I need only to check freq[number of occurencies]
+  */
+
+  for (let i = 0; i < arr.length; i += 1) {
+    const [action, value] = arr[i];
+    const initValue = dict[value] || 0;
+
+    if (action === 1) {
+      dict[value] = initValue + 1;
+
+      freq[initValue] = (freq[initValue] || 0) - 1;
+      freq[initValue + 1] = (freq[initValue + 1] || 0) + 1;
+    }
+
+    if (action === 2 && initValue > 0) {
+      dict[value] = initValue - 1;
+
+      freq[initValue - 1] += 1;
+      freq[initValue] -= 1;
+    }
+
+    if (action === 3) result.push(freq[value] > 0 ? 1 : 0);
   }
+
+  return result;
+}
 
 function main() {
     const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
