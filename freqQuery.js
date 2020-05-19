448d741506552b9b55f699e3c9a2f583f0cfc2db
Annie Liao <liaowow@gmail.com>
2020-05-19 19:53:05 -0400

    added another solution to freQuery challenge

--- freqQuery.js
+++ freqQuery.js
@@ -65,8 +65,8 @@ function freqQuery(queries) {
   
 }
 
-/*** Solution#1 ***/
-function freqQueryAll(arr) {
+/*** Solution 1 ***/
+function freqQueryAll1(arr) {
   const result = [];
   const dict = {};
   const freq = []; // keep an extra list of frequencies
@@ -100,6 +100,44 @@ function freqQueryAll(arr) {
   return result;
 }
 
+/*** Solution 2 ***/
+function freqQueryAll2(queries) {
+    const frequencies = [];
+    const frequencyTracker = [];
+    const result = [];
+
+    for (const query of queries) {
+        const action = query[0];
+        const value = query[1];
+        let index;
+
+        if (action === 1 || action === 2) {
+            index = frequencies[value];
+            frequencyTracker[index] ? --frequencyTracker[index] : null;
+        }
+
+        if (action === 1) {
+            typeof frequencies[value] === 'undefined' ? frequencies[value] = 1 : ++frequencies[value];
+        }
+
+        if (action === 2 && frequencies[value]) {
+            --frequencies[value];
+        }
+
+        if (action === 1 || action === 2) {
+            index = frequencies[value];
+            frequencyTracker[index] ? ++frequencyTracker[index] : frequencyTracker[index] = 1;
+        }
+
+        if (action === 3) {
+            result.push(frequencyTracker[value] > 0 ? 1 : 0);
+        }
+    }
+
+    return result;
+}
+
+
 function main() {
     const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
@@ -121,3 +159,4 @@ function main() {
 // resource
 // https://www.hackerrank.com/challenges/frequency-queries/forum
 // https://codereview.stackexchange.com/questions/202546/frequency-queries-hackerrank
+// https://ithelp.ithome.com.tw/articles/10226351
