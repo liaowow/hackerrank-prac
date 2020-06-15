483a565c412e3d4543988a4ed2c634ab8d261e8d
Annie Liao <liaowow@gmail.com>
2020-06-15 12:19:57 -0400

    added alt solutions to pyramid

--- udemy/pyramid.js
+++ udemy/pyramid.js
@@ -23,7 +23,8 @@ function pyramid(n) {
         // if middleIdx === ' ', set it to '#'
         // if middleIdx + i === ' ' && middleIdx - i === ' ', set them to '#'
         // console log array
-    const col = (n === 2) ? (n + 1) : (n + 2)
+    // const col = (n === 2) ? (n + 1) : (n + 2) -> my original
+    const col = n * 2 - 1
     const middleIdx = Math.floor(col / 2)
     let arr = Array(n).fill(' ') 
     for (let i = 0; i < arr.length; i++) {
@@ -42,5 +43,42 @@ function pyramid(n) {
 
 /* SOLUTION#1 */
 function pyramid1(n) {
-    
+    // determine number of columns (n * 2 - 1), find middleIdx
+    const midPoint = Math.floor((n * 2 - 1) / 2)
+    // iterate over each row
+    for (let row = 0; row < n; row++) {
+        // create empty str
+        let level = ''
+        // iterate over colomns
+        for (let col = 0; col < (n * 2 - 1); col++) {
+            if (midPoint - row <= col && midPoint + row >= column) {
+                level += '#'
+            } else {
+                level += ' '
+            }
+        }
+        console.log(level)
+    }
 }
+
+/* SOLUTION#2 -- using recursion */
+function pyramid2(n, row = 0, level = '') {
+    // base case
+    if (row === n) {
+        return;
+    }
+    
+    if (level.length === (2 * n - 1)) {
+        console.log(level)
+        return pyramid(n, row + 1)
+    }
+
+    const midPoint = Math.floor((2 * n - 1) / 2)
+    let add;
+    if (midPoint - row <= level.length && midPoint + row >= level.length) {
+        add = '#'
+    } else {
+        add = ' '
+    }
+    pyramid(n, row, level + add)
+} 
