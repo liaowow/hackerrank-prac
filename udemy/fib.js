bb7665bf3b3f55cfee7c466b484246ee51c50327
Annie Liao <liaowow@gmail.com>
2020-06-24 15:13:55 -0400

    added memoized solution to fib

--- udemy/fib.js
+++ udemy/fib.js
@@ -25,6 +25,7 @@ function fib(n) {
 }
 
 /* SOLUTION#1 -- iterative, initiates array with first two elements */
+// runtime complexity is linear, O(n)
 function fib1(n) {
     const result = [0, 1]
     
@@ -38,6 +39,59 @@ function fib1(n) {
 }
 
 /* SOLUTION#2 -- recursive */
+// runtime complexity is exponential, O(2^n):
+// for every additional element, we are getting a dramatic increase of operations
 function fib2(n) {
-    
+    if (n < 2) {
+        return n
+    }
+
+    return fib2(n - 1) + fib2(n - 2)
 }
+
+/* SOLUTION#3 -- Use MEMOIZATION to improve the recursive case:
+Store the argument of each function call along with the result.
+If the function is called again with the same args, 
+return the precomputed results (instead of running the funciton again).
+*/
+
+// create a memoizer to speed up fib function
+function memoize(fn) {
+    // key is argument we are passing in slowFib fn
+    // value is result of the function call
+    const cache = {}
+
+    // if only 1 arg
+    return function(x) {
+        if (cache[x]) {
+            return cache[x]
+        }
+
+        const result = fn(x)
+        cache[x] = result
+        return result
+    }
+
+    // if multiple args
+    return function(...args) {
+        if (cache[args]) {
+            return cache[args]
+        }
+
+        const result = fn(...args)
+        // const result = fn.apply(this, args)
+        cache[args] = result
+        return result
+    }
+
+}
+
+function fib(n) {
+    if (n < 2) {
+        return n
+    }
+
+    return fib(n - 1) + fib(n - 2)
+}
+
+fib = memoize(fib)
