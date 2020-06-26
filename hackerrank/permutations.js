5314a6731f62ae9cde094b0ca17b455f82092426
Annie Liao <liaowow@gmail.com>
2020-06-26 15:50:14 -0400

    added permutations

---
+++ hackerrank/permutations.js
@@ -0,0 +1,23 @@
+/* Description:
+Write an algorithm that takes an input string like 'abc', 
+and prints out all possible permutations of the string. 
+
+A permutation of a group of elements is one of the n! number possible arrangements 
+the elements can take, where n is the number of elements in the range.
+*/
+
+/* Example:
+const str = "abc"
+permutations(str)
+// abc
+// acb
+// bac
+// bca
+// cba
+// cab
+*/
+
+function permutations(str) {
+    // Fill in this method
+    return str;
+  }
