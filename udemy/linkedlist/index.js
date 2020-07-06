8b28d74a2c1c66c26898391ffebb6c08a0d375d5
Annie Liao <liaowow@gmail.com>
2020-07-06 10:15:12 -0400

    added my solutions for linkedlist's getAt and removeAt

--- udemy/linkedlist/index.js
+++ udemy/linkedlist/index.js
@@ -95,6 +95,68 @@ class LinkedList {
       // }
       // previous.next = null
   }
+
+  insertLast(data) {
+    let currentLastNode = this.getLast()
+
+    if (currentLastNode) {
+      currentLastNode.next = new Node(data)
+    } else {
+      this.head = new Node(data)
+    }
+  }
+
+  getAt(idx) {
+    // edge case#1: the chain is empty (actually, edge case#2 solution takes care of it)
+    if (!this.head) {
+        return null
+    }
+
+    let counter = 0
+    let node = this.head
+    while (node) {
+        if (counter === idx) {
+            return node
+        }
+        node = node.next
+        counter++
+    }
+    // edge case#2: idx doesn't exist in this chain
+    return null
+  }
+
+  removeAt(idx) {
+    /* tutorial */
+    if (!this.head) {
+      return
+    }
+
+    if (idx === 0) {
+        this.head = this.head.next
+    }
+
+    const prevNode = this.getAt(idx - 1)
+    // edge cases: when idx is out of bounds
+    if (!prevNode || !prevNode.next) {
+        return
+    }
+    prevNode.next = prevNode.next.next
+
+    /* my solution, less performant */
+    // if (!this.head) {
+    //   return
+    // }
+    
+    // let prevNode = this.getAt(idx - 1)
+    // let nextNode = this.getAt(idx + 1)
+    
+    // if (idx === 0) {
+    //   this.head = nextNode
+    //   return
+    // }
+    
+    // prevNode.next = nextNode
+  }
 }
 
 module.exports = { Node, LinkedList };
