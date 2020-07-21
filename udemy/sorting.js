2a3259ee7e62b877680d4f50d682d24968b68495
Annie Liao <liaowow@gmail.com>
2020-07-21 11:31:07 -0400

    added merge function

--- udemy/sorting.js
+++ udemy/sorting.js
@@ -40,7 +40,16 @@ function mergeSort(arr) {
 }
 
 function merge(left, right) {
-
+  const results = []
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift())
+    } else {
+      results.push(right.shift())
+    }
+  }
+  // ES6 syntax for joining multiple elements from different arrays into a new array
+  return [...results, ...left, ...right]
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
