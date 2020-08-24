4e464aa9f72a99bda7f46a4735f8872551483eff
Annie Liao <liaowow@gmail.com>
2020-08-23 20:14:04 -0400

    added bst preorder traversal

--- others/bstOrders.js
+++ others/bstOrders.js
@@ -28,3 +28,49 @@ function findInOrderSuccessor(inputNode) {
 }
 
 // resource: https://medium.com/swlh/how-to-solve-a-js-binary-search-tree-problem-585673fc3287
+
+/**
+ Preorder:
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+function preOrderTraversel(root) {
+  let stack = []
+  
+  // We do not push the root node onto the stack if the root node is null. This way we will avoid going into the while loop when the root is null and just return an empty array as the result. 
+  if (root !== null){
+    stack.push(root)
+  }
+
+  // Initialize the result to an empty array 
+  let res = [];
+
+  // Keep iterating while there is something on the stack
+  while (stack.length > 0) {
+    const node = stack.pop();
+    
+    // Do the preorder processing
+    res.push(node.val);
+    
+    // If there is a right child, push it onto the stack. 
+    if (node.right !== null) {
+        stack.push(node.right);
+    }
+    
+    // If there is a left child, push it onto the stack. 
+    if (node.left !== null) {
+        stack.push(node.left);
+    }
+  }
+  return res; 
+}
+
+ // resource: https://leetcode.com/problems/binary-tree-preorder-traversal/discuss/177117/Simple-Iterative-Javascript-Solution
