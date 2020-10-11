acf818d26224297c935d33a8c4e049983c60a11c
Annie Liao <liaowow@gmail.com>
2020-10-11 17:37:17 -0400

    add powerset recursive solution

--- expert/algo/powerset.js
+++ expert/algo/powerset.js
@@ -1,4 +1,4 @@
-// iterative solution: O(n^2 n) time && space complexity
+// iterative solution: O(n^2 * n) time && space complexity
 function powerset(arr) {
   let result = [[]]
   // (1) iterate over arr
@@ -12,5 +12,27 @@ function powerset(arr) {
       result.push(currSubset.concat(arr[i]))
     }
   }
+  return result
+}
+
+// recursive solution: O(n^2 * n) time && space complexity
+function powersetRecursive(arr, idx = null) {
+  // initial setting: assign idx to the idx of last element
+  if (idx === null) {
+    idx = arr.length - 1
+  }
+  // base case
+  if (idx < 0) {
+    return [[]]
+  }
+
+  const lastEle = arr[idx]
+  const result = powersetRecursive(arr, idx - 1)
+  const currLeng = result.length
+  for (let i = 0; i < currLeng; i++) {
+    const currSubset = result[i]
+    result.push(currSubset.concat(lastEle))
+  }
+
   return result
 }
