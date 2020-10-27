e1daf10dd3b1b95aabd0c08276242c09eb0661c6
Annie Liao <liaowow@gmail.com>
2020-10-27 14:45:19 -0400

    modify validSubsequence solution

--- expert/algo/validSubsequence.js
+++ expert/algo/validSubsequence.js
@@ -5,13 +5,11 @@ function isValidSubsequence(arr, sequence) {
 	if (arr.length === 1 && sequence.length === 1 && arr[0] === sequence[0]) return true
 	
 	for (let i = 0; i < arr.length; i++) {
+		if (currSeqIdx === sequence.length) break
 		if (arr[i] === sequence[currSeqIdx]) {
 			currSeqIdx++
 		}
-		if (currSeqIdx === sequence.length - 1 && arr[i] === sequence[currSeqIdx]) {
-			return true
-		}
 	}
 	
-	return false
+	return currSeqIdx === sequence.length
 }
