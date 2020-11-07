b7529926fe2d04231555ef08314168221aa296ff
Annie Liao <liaowow@gmail.com>
2020-11-07 13:27:46 -0500

    add dp problem

---
+++ expert/algo/dp/maxSubSum.js
@@ -0,0 +1,8 @@
+/* 
+Write a function that takes in an array of positive integers and returns the max sum of
+non-adjacent elements in the array.
+If a sum can't be generated, return 0.
+*/
+function maxSubsetSumNoAdjacent(array) {
+
+}
