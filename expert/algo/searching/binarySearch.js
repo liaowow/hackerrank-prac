d20030c8e9c11d43c494d03796d6d5b64b539424
Annie Liao <liaowow@gmail.com>
2021-06-25 18:55:26 -0400

    add recursive solution to binarySearch and add time space complexity

--- expert/algo/searching/binarySearch.js
+++ expert/algo/searching/binarySearch.js
@@ -1,3 +1,4 @@
+// O(log(n)) time, O(1) space
 function binarySearch(arr, target) {
   let left = 0
 	let right = arr.length - 1
@@ -14,4 +15,19 @@ function binarySearch(arr, target) {
 		}
 	}
 	return -1;
+}
+
+// O(log(n)) time, O(log(n)) space
+function binarySearchRecursive(arr, target, left = 0, right = arr.length - 1) {
+	if (left > right) return -1;
+  
+  const middle = (Math.floor((left + right) / 2))
+  const targetInt = arr[middle]
+  if (target === targetInt) {
+    return middle
+  } else if (target < targetInt) {
+    return binarySearchRecursive(arr, target, left, middle - 1)
+  } else {
+    return binarySearchRecursive(arr, target, middle + 1, right)
+  }
 }
