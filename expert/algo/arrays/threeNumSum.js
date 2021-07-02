cd3b0a9ad9d79c8f6e202713cce156b3d2454947
Annie Liao <liaowow@gmail.com>
2021-07-02 12:31:52 -0400

    complete practice of threeNumSum using two-pointer technique

--- expert/algo/arrays/threeNumSum.js
+++ expert/algo/arrays/threeNumSum.js
@@ -1,5 +1,28 @@
+// O(n^2) time, O(n) space
 function threeNumSum(arr, target) {
-  let result = []
-  arr.sort((a, b) => a - b)
+	let result = []
+	
+	arr.sort((a, b) => a - b)
+	for (let i = 0; i < arr.length - 2; i++) {
+		const currNum = arr[i]
+		let left = i + 1
+		let right = arr.length - 1
 
+		while (left < right) {
+			const currSum = currNum + arr[left] + arr[right]
+			if (currSum === target) {
+				result.push([currNum, arr[left], arr[right]])
+				left++
+				right--
+			} 
+			if (currSum < target) {
+				left++
+			} 
+			if (currSum > target) {
+				right--
+			}
+		}
+	}
+	
+	return result
 }
